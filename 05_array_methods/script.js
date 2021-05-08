725ea43ae753af9cd812e1a7ab7f9ad55292e113
hotcoffeehero <hotcoffeehero@gmail.com>
2021-05-08 09:40:51 +0900

    05 forEach add_user

--- 05_array_methods/script.js
+++ 05_array_methods/script.js
@@ -7,12 +7,6 @@ const calc_wealth_btn = document.getElementById('calculate-wealth')
 
 let data = []
 
-//Add new object to the data array
-
-let add_data = async (obj) => {
-  data.push(obj)
-}
-
 //Fetch a random user and add money
 
 let get_ran_user = async () => {
@@ -31,6 +25,37 @@ let get_ran_user = async () => {
   add_data(new_user)
 }
 
+//Add new object to the data array
+
+let add_data = async (obj) => {
+  data.push(obj)
+
+  update_DOM()
+}
+
+// Update DOM with new random data
+let update_DOM = (provided_data = data) => {
+  //clear what's in the main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>'
+  provided_data.forEach((item) => {
+    const element = document.createElement('div')
+    element.classList.add('person')
+    element.innerHTML = `<strong>${item.name}</strong>${format_money(
+      item.money
+    )}`
+    main.appendChild(element)
+  })
+}
+
+//Format the number as money
+let format_money = (number) => {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
+}
+
 get_ran_user()
 get_ran_user()
 get_ran_user()
+
+//Event listeners
+
+add_user_btn.addEventListener('click', get_ran_user)
